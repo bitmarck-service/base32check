641d653d8e6815b5cb9e81adaad01e35a8ecefbc
Christian Schlichtherle <christian@schlichtherle.de>
2020-05-07 18:55:36 +0200

    Add edit page links.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,11 @@
 module.exports = {
     description: 'Your source for the Base32 and Base32Check1 algorithms',
     themeConfig: {
+        docsBranch: 'develop',
+        docsDir: 'docs',
+        editLinks: true,
+        editLinkText: 'Help us improve this page!',
+        repo: 'bitmarck-service/base32check',
         sidebar: [
             '/',
             ['java', 'Java'],
